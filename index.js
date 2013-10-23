1d18ba6e5209da08797e5d14c434161642318d05
bredele <olivier.wietrich@gmail.com>
2013-10-22 21:27:32 -0600

    add lock and unlock functions, both can accept arguments

---
+++ index.js
@@ -0,0 +1,41 @@
+var Emitter = require('emitter');
+
+/**
+ * Expose 'Doors'
+ */
+
+module.exports = Doors;
+
+
+/**
+ * Doors constructor.
+ * @api public
+ */
+
+function Doors() {
+  //do something
+}
+
+
+//Doors is an emitter
+Emitter(Doors.prototype);
+
+
+/**
+ * [lock description]
+ * @return {[type]} [description]
+ */
+
+Doors.prototype.lock = function() {
+	
+};
+
+
+/**
+ * [lock description]
+ * @return {[type]} [description]
+ */
+
+Doors.prototype.unlock = function() {
+	
+};
