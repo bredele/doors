537031b675a4222a49218935aee440d28e54b4cd
bredele <olivier.wietrich@gmail.com>
2014-04-25 12:23:14 -0600

    refactor door and tst

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+/**
+ * Module dependencies.
+ * @api private
+ */
+
 var Emitter = require('component-emitter');
 var index = require('indexof');
 
@@ -15,9 +21,9 @@ module.exports = Doors;
  */
 
 function Doors(name, locks) {
-	var arr = locks || [];
-	this.name = name;
-	this.keys = [];
+  var arr = locks || [];
+  this.name = name;
+  this.keys = [];
   this.locks = {};
   for(var l = arr.length; l--;) {
     this.add(arr[l]);
@@ -25,7 +31,7 @@ function Doors(name, locks) {
 }
 
 
-//Doors is an emitter
+//inherits Emitter
 
 Emitter(Doors.prototype);
 
@@ -39,65 +45,75 @@ Emitter(Doors.prototype);
  */
 
 Doors.prototype.has = function(key) {
-	return !!~index(this.keys, key);
+  return !!~index(this.keys, key);
 };
 
 
 /**
  * Add lock.
  *
+ * Examples:
+ *
+ *   door.add('lock1');
+ *
  * @param {String} name 
+ * @return {this}
  * @api public
  */
 
 Doors.prototype.add = function(lock) {
-	if(!this.has(lock)) {
-		var key = lock;
-		if(lock instanceof Doors) {
-			var _this = this;
-			key = lock.name;
-			lock.on('open', function() {
-				_this.unlock(key);
-			});
-		}
-		this.locks[key] = lock;
-		this.keys.push(key);
-	}
+  if(!this.has(lock)) {
+    var key = lock;
+    if(lock instanceof Doors) {
+      var _this = this;
+      key = lock.name;
+      lock.on('open', function() {
+        _this.unlock(key);
+      });
+    }
+    this.locks[key] = lock;
+    this.keys.push(key);
+  }
+  return this;
 };
 
 
 /**
  * Lock a previously added lock.
+ * 
  * Examples:
  *
  *     door.lock('olivier'); //lock 'olivier'
  *     door.lock('olivier', 'amy'); //lock 'olivier' and 'amy'
  *     door.lock(); //lock all the locks
  *
+ * @return {this}
  * @api public
  */
 
 Doors.prototype.lock = function() {
-	var length = arguments.length;
-	if(length) {
-		for(var l = length; l--;) {
-			var key = arguments[l];
-			if(this.locks[key] && !this.has(key)) {
-				var lock = this.locks[key];
-				if(lock instanceof Doors) {
-					lock.lock();
-				}
-				this.keys.push(key);
-			}
-		}
-	} else if(Object.keys(this.locks).length){
-		this.lock.apply(this, Object.keys(this.locks));
-	}
+  var length = arguments.length;
+  if(length) {
+    for(var l = length; l--;) {
+      var key = arguments[l];
+      if(this.locks[key] && !this.has(key)) {
+        var lock = this.locks[key];
+        if(lock instanceof Doors) {
+          lock.lock();
+        }
+        this.keys.push(key);
+      }
+    }
+  } else if(Object.keys(this.locks).length){
+    this.lock.apply(this, Object.keys(this.locks));
+  }
+  return this;
 };
 
 
 /**
  * Unlock door's lock(s).
+ * 
  * Examples:
  *
  *     door.unlock('olivier'); //unlock 'olivier'
@@ -105,43 +121,51 @@ Doors.prototype.lock = function() {
  *     door.unlock(); //unlock all the locks
  *
  * @params {String} key(s)
+ * @return {this} 
  * @api public
  */
 
 Doors.prototype.unlock = function() {
-	var length = arguments.length;
-	if(length) {
-		for(var l = length; l--;) {
-			var key = arguments[l];
-			if(this.has(key)) {
-				var lock = this.locks[key];
-				this.keys.splice(index(this.keys, key), 1);
-				if(lock instanceof Doors) {
-					lock.unlock();
-				}
-				this.open();
-			}
-		}
-	} else if(this.keys.length){
-		this.unlock.apply(this, this.keys);
-	}
+  var length = arguments.length;
+  if(length) {
+    for(var l = length; l--;) {
+      var key = arguments[l];
+      if(this.has(key)) {
+        var lock = this.locks[key];
+        this.keys.splice(index(this.keys, key), 1);
+        if(lock instanceof Doors) {
+          lock.unlock();
+        }
+        this.open();
+      }
+    }
+  } else if(this.keys.length){
+    this.unlock.apply(this, this.keys);
+  }
+  return this;
 };
 
 
 /**
  * Toggle Lock.
+ *
+ * Examples:
+ *
+ *   // unlock
+ *   door.toggle('lock1', true);
+ *   //lock
+ *   door.toogle('lock1');
  * 
  * @param  {String} name 
  * @param  {Boolean} bool 
+ * @return {this}
  * @api public
  */
 
 Doors.prototype.toggle = function(name, bool) {
-	if(bool) {
-		this.unlock(name);
-	} else {
-		this.lock(name);
-	}
+  if(bool) this.unlock(name);
+  else this.lock(name);
+  return this;
 };
 
 
@@ -154,9 +178,9 @@ Doors.prototype.toggle = function(name, bool) {
  */
 
 Doors.prototype.open = function() {
-	if(!this.keys.length) {
-		this.emit('open');
-		return true;
-	}
-	return false;
+  if(!this.keys.length) {
+    this.emit('open');
+    return true;
+  }
+  return false;
 };
--- test/doors.js
+++ test/doors.js
@@ -1,8 +1,12 @@
+
+/**
+ * Test dependencies.
+ */
+
 var Doors = require('..');
 var assert = require('assert');
 
-//utils
-
+// utils
 
 var contains = function(arr, val) {
   return !!~arr.indexOf(val);
@@ -20,43 +24,48 @@ var has = function(obj, key) {
   return !!obj[key];
 };
 
-describe('Constructor', function(){
+describe('Constructor', function() {
+
   it("should initalize a door with a name", function() {
     var door = new Doors('bredele');
-    assert(door.name === 'bredele');
+    assert.equal(door.name, 'bredele');
   });
 
   it('should initialize a door with a name and an array of locks', function() {
     var door = new Doors('bredele', ['olivier', 'amy']);
-    assert(contains(door.keys, 'olivier') === true);
-    assert(contains(door.keys, 'amy') === true);
+    assert.equal(contains(door.keys, 'olivier'), true);
+    assert.equal(contains(door.keys, 'amy'), true);
   });
+
 });
 
 
 describe("Add lock", function() {
+
   it("should add lock which is locked by default", function() {
     var door = new Doors('bredele');
     door.add('olivier');
-    assert(contains(door.keys, 'olivier') === true);
+    assert.equal(contains(door.keys, 'olivier'), true);
   });
   
   it("should not add a lock twice", function() {
     var door = new Doors('bredele');
     door.add('olivier');
     door.add('olivier');
-    assert(count(door.keys, 'olivier') === 1);
+    assert.equal(count(door.keys, 'olivier'), 1);
   });
   
 });
 
 
-describe('has locked lock', function(){
+describe('has locked lock', function() {
+
   it("returns true if door contains locked lock", function() {
     var door = new Doors('bredele');
     door.add('olivier');
-    assert(door.has('olivier') === true);
+    assert.equal(door.has('olivier'), true);
   });
+
 });
 
 
@@ -69,42 +78,42 @@ describe("Unlock", function() {
   
   it("should unlock a lock previously added", function() {
     door.unlock('olivier');
-    assert(contains(door.keys, 'olivier') === false);
+    assert.equal(contains(door.keys, 'olivier'), false);
   });
 
   it('should unlock multiple locks', function() {
     door.add('amy');
     door.unlock('olivier', 'amy');
-    assert(contains(door.keys, 'olivier') === false);
-    assert(contains(door.keys, 'amy') === false);
+    assert.equal(contains(door.keys, 'olivier'), false);
+    assert.equal(contains(door.keys, 'amy'), false);
   });
 
   it('should unlock the entire door if no arguments are passed', function() {
     door.add('amy');
     door.unlock();
-    assert(contains(door.keys, 'olivier') === false);
-    assert(contains(door.keys, 'amy') === false);
+    assert.equal(contains(door.keys, 'olivier'), false);
+    assert.equal(contains(door.keys, 'amy'), false);
   });
 
   it('should not unlock an unknown lock', function() {
     door.add('amy');
     door.unlock('bruno');
-    assert(contains(door.keys, 'olivier') === true);
-    assert(contains(door.keys, 'amy') === true);
+    assert.equal(contains(door.keys, 'olivier'), true);
+    assert.equal(contains(door.keys, 'amy'), true);
   });
 
   describe("open event", function() {
-    it('should emit an open event if the every locks are unlocked', function() {
+
+    it('should emit an open event if the every locks are unlocked', function(done) {
       var isOpen = false;
       door.add('amy');
       door.on('open', function() {
-        isOpen = true;
+        done();
       });
       door.unlock();
-      assert(isOpen === true);
     });
+
   });
-  
 
 });
 
@@ -118,30 +127,30 @@ describe("Lock", function() {
   });
 
   it("should lock a lock previously added", function() {
-    assert(contains(door.keys, 'olivier') === true);
+    assert.equal(contains(door.keys, 'olivier'), true);
   });
 
   it('should lock a lock previously unlocked', function() {
     door.unlock('olivier');
-    assert(contains(door.keys, 'olivier') === false);
+    assert.equal(contains(door.keys, 'olivier'), false);
     door.lock('olivier');
-    assert(contains(door.keys, 'olivier') === true);
+    assert.equal(contains(door.keys, 'olivier'), true);
   });
 
   it('should lock multiple locks', function() {
     door.unlock('olivier');
     door.unlock('amy');
     door.lock('olivier', 'amy');
-    assert(contains(door.keys, 'olivier') === true);
-    assert(contains(door.keys, 'amy') === true);
+    assert.equal(contains(door.keys, 'olivier'), true);
+    assert.equal(contains(door.keys, 'amy'), true);
   });
 
   it('should lock the entire door if no arguments', function() {
     door.unlock('olivier');
     door.unlock('amy');
     door.lock();
-    assert(contains(door.keys, 'olivier') === true);
-    assert(contains(door.keys, 'amy') === true);
+    assert.equal(contains(door.keys, 'olivier'), true);
+    assert.equal(contains(door.keys, 'amy'), true);
   });
 
   // describe("close event", function() {
@@ -173,30 +182,35 @@ describe("Lock", function() {
 
 
 describe("Toggle Lock", function() {
+
   it("unlocks if second argument if truthy", function() {
     var door = new Doors('bredele');
     door.add('olivier');
     door.toggle('olivier', true);
-    assert(contains(door.keys, 'olivier') === false);
+    assert.equal(contains(door.keys, 'olivier'), false);
   });
+
   it("locks if second argument if falsy", function() {
     var door = new Doors('bredele');
     door.add('olivier');
     door.unlock('olivier');
     door.toggle('olivier', false);
-    assert(contains(door.keys, 'olivier') === true);
+    assert.equal(contains(door.keys, 'olivier'), true);
   });
+
 });
 
 
 describe("Open", function() {
+
   it("returns true if door is open", function() {
     var door = new Doors('bredele');
     door.add('olivier');
-    assert(door.open() === false);
+    assert.equal(door.open(), false);
     door.unlock('olivier');
-    assert(door.open() === true);
+    assert.equal(door.open(), true);
   });
+  
 });
 
 
@@ -209,19 +223,17 @@ describe("Door inception", function() {
   it("should add a door", function() {
     var lock = new Doors('github');
     door.add(lock);
-    assert(has(door.locks, 'github') === true);
+    assert.equal(has(door.locks, 'github'), true);
   });
 
-  it("should unlock child door if unlocked from parent door", function() {
-    var isOpen = false;
+  it("should unlock child door if unlocked from parent door", function(done) {
     var lock = new Doors('lock1');
     lock.add('olivier');
     door.add(lock);
     lock.on('open', function() {
-      isOpen = true;
+      done();
     });
     door.unlock('lock1');
-    assert(isOpen === true);
   });
 
   it('shoud unlock child in parent if child is openned', function() {
@@ -229,18 +241,17 @@ describe("Door inception", function() {
     lock.add('olivier');
     door.add(lock);
     lock.unlock();
-    assert(door.has('lock2') === false);
+    assert.equal(door.has('lock2'), false);
   });
 
   it("should lock child door if locked from parent door", function() {
     var lock = new Doors('lock3');
-    debugger
     door.add(lock);
     door.unlock('lock3');
     lock.add('olivier');
     lock.unlock();
     door.lock('lock3');
-    assert(lock.has('olivier') === true);
+    assert.equal(lock.has('olivier'), true);
 
   });
 
@@ -251,7 +262,7 @@ describe("Door inception", function() {
 
     door.add(lock);
     lock.lock();
-    assert(door.has('lock2') === true);
+    assert.equal(door.has('lock2'), true);
   });
   
 });
