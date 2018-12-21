9fe1031ad315fdd13ff858176a6fc068e4e38fb0
bredele <olivier.wietrich@gmail.com>
2018-12-21 10:46:35 -0700

    Return promise for a lock

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+/**
+ * Dependency
+ */
+
+var emitter = require('component-emitter')
+
 
 module.exports = function () {
   const door = {}
 
-  door.promise = function () {
-
+  door.promise = function (name) {
+    return new Promise(function (resolve, reject) {
+      door.once('lock ' + name, reject)
+      door.once('unlock ' + name, resolve)
+    })
   }
 
-  door.lock = function () {
-
+  door.lock = function (name) {
+    door.emit('lock ' + name)
   }
 
-  door.unlock = function () {
-
+  door.unlock = function (name) {
+    door.emit('unlock ' + name)
   }
-  
-  return door
+
+  return emitter(door)
 }
--- test/doors.js
+++ test/doors.js
@@ -13,7 +13,21 @@ test('should have the following API', assert => {
   assert.equal(typeof door.lock, 'function')
   assert.equal(typeof door.unlock, 'function')
 })
-// 
-// test('should ', assert => {
-//
-// })
+
+test('should be an event emitter', assert => {
+  assert.plan(3)
+  var door = doors()
+  assert.equal(typeof door.on, 'function')
+  assert.equal(typeof door.emit, 'function')
+  assert.equal(typeof door.once, 'function')
+})
+
+test('should add lock and resolve promise only once when unlocked', assert => {
+  assert.plan(1)
+  var door = doors()
+  door.lock('hello')
+  door.promise('hello').then(() => assert.ok('hello'))
+  door.unlock('hello')
+})
+
+//test('should resolve only once')
